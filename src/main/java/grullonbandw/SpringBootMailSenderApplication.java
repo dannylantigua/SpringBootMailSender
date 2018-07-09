720e59fa2fe3942b1397a41042c1ad49d3085d9a
Danny Lantigua <dannygrullon7@gmail.com>
2018-07-09 13:50:53 -0400

    First commit, project structure created

---
+++ src/main/java/grullonbandw/SpringBootMailSenderApplication.java
@@ -0,0 +1,12 @@
+package grullonbandw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMailSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMailSenderApplication.class, args);
+	}
+}
